570d3f19361e0cc1b528bd401cc602ca88bc451e
JQSP <joaoquintiliano8@gmail.com>
2021-11-11 21:47:57 -0200

    ResponsivTelaAceitarProf

--- resources/views/autorizeTeacher.blade.php
+++ resources/views/autorizeTeacher.blade.php
@@ -7,127 +7,63 @@
 
 <main>
     <hr>
-    <div class="container-fluid row row-cols-3">
-        <div class="col container">
-            <p>Nome: João Quintiliano de Souza Pontes</p> <br>
-            <p>Org: Instituto Federal do Rio Grande do Norte</p>
+    <div class="container-fluid">
+        <div id="campo" class="text-center ">
+            <div>
+                <p><b>Nome: </b>João Quintiliano de Souza Pontes</p>
+            </div>
+            <div>
+                <p><b>Org: </b>Instituto Federal do Rio Grande do Norte</p>
+            </div>
+            <div>
+                <p><b>Lattes:</b> https://www.lattesdousuario.com.br</p>
+            </div>
         </div>
-        <div class="col container">
-            <p>Lattes: https://www.lattesdousuario.com.br</p>
-        </div>
-        <div class="text-right container">
+        <div class="text-center container">
             <button> <img class="icon img-fluid " src="/img/aprovar.png" alt="Icone retirado do: https://www.flaticon.com/br/"></button>
             <button> <img class="icon img-fluid " src="/img/recusado.png" alt="Icone retirado do: https://www.flaticon.com/br/"></button>
         </div>
     </div>
     <hr>
     <hr>
-    <div class="container-fluid row row-cols-3">
-        <div class="col container">
-            <p>Nome: João Quintiliano de Souza Pontes</p> <br>
-            <p>Org: Instituto Federal do Rio Grande do Norte</p>
+    <div class="container-fluid">
+        <div id="campo" class="text-center ">
+            <div>
+                <p><b>Nome: </b>João Quintiliano de Souza Pontes</p>
+            </div>
+            <div>
+                <p><b>Org: </b>Instituto Federal do Rio Grande do Norte</p>
+            </div>
+            <div>
+                <p><b>Lattes:</b> https://www.lattesdousuario.com.br</p>
+            </div>
         </div>
-        <div class="col container">
-            <p>Lattes: https://www.lattesdousuario.com.br</p>
-        </div>
-        <div class="text-right container">
+        <div class="text-center container">
             <button> <img class="icon img-fluid " src="/img/aprovar.png" alt="Icone retirado do: https://www.flaticon.com/br/"></button>
             <button> <img class="icon img-fluid " src="/img/recusado.png" alt="Icone retirado do: https://www.flaticon.com/br/"></button>
         </div>
     </div>
     <hr>
     <hr>
-    <div class="container-fluid row row-cols-3">
-        <div class="col container">
-            <p>Nome: João Quintiliano de Souza Pontes</p> <br>
-            <p>Org: Instituto Federal do Rio Grande do Norte</p>
+    <div class="container-fluid">
+        <div id="campo" class="text-center ">
+            <div>
+                <p><b>Nome: </b>João Quintiliano de Souza Pontes</p>
+            </div>
+            <div>
+                <p><b>Org: </b>Instituto Federal do Rio Grande do Norte</p>
+            </div>
+            <div>
+                <p><b>Lattes:</b> https://www.lattesdousuario.com.br</p>
+            </div>
         </div>
-        <div class="col container">
-            <p>Lattes: https://www.lattesdousuario.com.br</p>
-        </div>
-        <div class="text-right container">
+        <div class="text-center container">
             <button> <img class="icon img-fluid " src="/img/aprovar.png" alt="Icone retirado do: https://www.flaticon.com/br/"></button>
             <button> <img class="icon img-fluid " src="/img/recusado.png" alt="Icone retirado do: https://www.flaticon.com/br/"></button>
         </div>
     </div>
     <hr>
-    <hr>
-    <div class="container-fluid row row-cols-3">
-        <div class="col container">
-            <p>Nome: João Quintiliano de Souza Pontes</p> <br>
-            <p>Org: Instituto Federal do Rio Grande do Norte</p>
-        </div>
-        <div class="col container">
-            <p>Lattes: https://www.lattesdousuario.com.br</p>
-        </div>
-        <div class="text-right container">
-            <button> <img class="icon img-fluid " src="/img/aprovar.png" alt="Icone retirado do: https://www.flaticon.com/br/"></button>
-            <button> <img class="icon img-fluid " src="/img/recusado.png" alt="Icone retirado do: https://www.flaticon.com/br/"></button>
-        </div>
-    </div>
-    <hr>
-    <hr>
-    <div class="container-fluid row row-cols-3">
-        <div class="col container">
-            <p>Nome: João Quintiliano de Souza Pontes</p> <br>
-            <p>Org: Instituto Federal do Rio Grande do Norte</p>
-        </div>
-        <div class="col container">
-            <p>Lattes: https://www.lattesdousuario.com.br</p>
-        </div>
-        <div class="text-right container">
-            <button> <img class="icon img-fluid " src="/img/aprovar.png" alt="Icone retirado do: https://www.flaticon.com/br/"></button>
-            <button> <img class="icon img-fluid " src="/img/recusado.png" alt="Icone retirado do: https://www.flaticon.com/br/"></button>
-        </div>
-    </div>
-    <hr>
-    <hr>
-    <div class="container-fluid row row-cols-3">
-        <div class="col container">
-            <p>Nome: João Quintiliano de Souza Pontes</p> <br>
-            <p>Org: Instituto Federal do Rio Grande do Norte</p>
-        </div>
-        <div class="col container">
-            <p>Lattes: https://www.lattesdousuario.com.br</p>
-        </div>
-        <div class="text-right container">
-            <button> <img class="icon img-fluid " src="/img/aprovar.png" alt="Icone retirado do: https://www.flaticon.com/br/"></button>
-            <button> <img class="icon img-fluid " src="/img/recusado.png" alt="Icone retirado do: https://www.flaticon.com/br/"></button>
-        </div>
-    </div>
-    <hr>
-    <hr>
-    <div class="container-fluid row row-cols-3">
-        <div class="col container">
-            <p>Nome: João Quintiliano de Souza Pontes</p> <br>
-            <p>Org: Instituto Federal do Rio Grande do Norte</p>
-        </div>
-        <div class="col container">
-            <p>Lattes: https://www.lattesdousuario.com.br</p>
-        </div>
-        <div class="text-right container">
-            <button> <img class="icon img-fluid " src="/img/aprovar.png" alt="Icone retirado do: https://www.flaticon.com/br/"></button>
-            <button> <img class="icon img-fluid " src="/img/recusado.png" alt="Icone retirado do: https://www.flaticon.com/br/"></button>
-        </div>
-    </div>
-    <hr>
-    <hr>
-    <div class="container-fluid row row-cols-3">
-        <div class="col container">
-            <p>Nome: João Quintiliano de Souza Pontes</p> <br>
-            <p>Org: Instituto Federal do Rio Grande do Norte</p>
-        </div>
-        <div class="col container">
-            <p>Lattes: https://www.lattesdousuario.com.br</p>
-        </div>
-        <div class="text-right container">
-            <button> <img class="icon img-fluid " src="/img/aprovar.png" alt="Icone retirado do: https://www.flaticon.com/br/"></button>
-            <button> <img class="icon img-fluid " src="/img/recusado.png" alt="Icone retirado do: https://www.flaticon.com/br/"></button>
-        </div>
-    </div>
-    <hr>
-
-
 </main>
+
 
 @endsection
