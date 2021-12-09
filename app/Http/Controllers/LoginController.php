30e2924e6873ac27553f6c23458ae2387c53ccd6
luan.carmo <luan.carmo@escolar.ifrn.edu.br>
2021-12-09 17:37:03 -0300

    Views do admin e do professor mostrando os dados

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -34,15 +34,15 @@ class LoginController extends Controller
 
         $estudante = Student::where('email', $email)->first();
         if ($estudante != null && Hash::check($senha, $estudante->password)) {
-            session(['usuario' => $estudante, 'tipo' => 'estudante']);
+            session(['usuario' => $estudante, 'tipo' => 'estudante', 'nome' => $estudante->nome]);
         } else {
             $professor = Teacher::where('email', $email)->first();
             if ($professor != null && Hash::check($senha, $professor->password)) {
-                session(['usuario' => $professor, 'tipo' => 'professor']);
+                session(['usuario' => $professor, 'tipo' => 'professor', $professor->nome]);
             } else {
                 $admin = Admin::where('email', $email)->first();
                 if ($admin != null && Hash::check($senha, $admin->password)) {
-                    session(['usuario' => $admin, 'tipo' => 'admin']);
+                    session(['usuario' => $admin, 'tipo' => 'admin', $admin->nome]);
                 }
             }
         }
@@ -56,37 +56,4 @@ class LoginController extends Controller
         session()->flush();
         return redirect('/');
     }
-    
-    /*
-    public function checkTeacherPassword(Teacher $professor){
-        
-        $professor = new Teacher;
-
-        $professor->email = $request->email;
-        $professor->password = $request->password;
-
-        $teachersEmail = DB::table('teachers')->pluck('email');
-        $teachersPass = DB::table('teachers')->pluck('password');
-        
-        $teachersPass = DB::table('teachers')->pluck('password');
-
-        foreach($teachersPass as $senhaProfessor){
-            if(password_verify($professor->password, $senhaProfessor)){
-                return redirect('/professor');
-            }
-        }
-
-        return redirect('/')->with('msg','Email ou Senha Incorretos!'); 
-    }
-
-    public function checkStudentPassword(Student $student){
-        $studentPass = DB::table('students')->pluck('password');
-
-        foreach($studentsPass as $senhaEstudante){
-            if(password_verify($studante->password, $senhaEstudante)){
-                return redirect('/estudante');
-            }
-        }
-    }
-    */
 }
